0e3cc77e326301d75fdc4187f151e60ed803161a
Syrniellow <michele.cernigliaro@outlook.com>
2023-08-03 00:41:55 +0200

    Fixed blog Post model

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,9 @@ from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
-    models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
